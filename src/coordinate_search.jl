6700b857d01a0e792d1fd5d42eeda1192263cf54
joaoluisro <48219313+joaoluisro@users.noreply.github.com>
2019-07-15 13:35:11 -0300

    Update coordinate_search.jl
    Fixed indentation, added β as a option and corrected a few mistakes.

--- src/coordinate_search.jl
+++ src/coordinate_search.jl
@@ -2,64 +2,74 @@ export coordinate_search
 
 """
 	The coordinate search is a derivative free optimization method.
-	It minimizes a certain function f by walking on a grid and taking a step where the function values are smaller.
-	When it can no longer find any smaller values, it reduces the grid by a factor of 2, repeating the process.
-
+	It minimizes a certain function f by walking on a grid and taking steps towards smaller function values.
+	When it can no longer find any smaller values, it reduces the grid by a factor of β, repeating the process.
+	coordinate_search(nlp; options...)
+	where "nlp" is an "AbstractNLPModel".
+	standard options :
+	tol = 1e-12
+	α = 1.0
+	β = 2.0
+	max_time = 1e-1
+	max_eval = 500
 """
 
 function coordinate_search(nlp :: AbstractNLPModel;
-			   tol = 1e-12,
-			   k = 0,
-			   α = 1.0,
-			   xk = nlp.meta.x0,
-			   el_time = 0.0,
+			   tol :: Real = 1e-12,
+			   α :: Real = 1.0,
+			   β :: Real = 2.0,
+			   x :: AbstractVector = nlp.meta.x0,
 			   max_time :: Real  = 1e-1,
-			   max_eval = 500)
+			   max_eval :: Int = 500)
+	
+  start_time = time()
+  el_time = 0.0
+  tired = neval_obj(nlp) > max_eval || el_time > max_time
+  optimal = α <= tol
+  T = eltype(x)
+  status =:unknown
+  k = 0
+  @info log_header([:iter, :f, :H], [Int, T, T], hdr_override=Dict(:f=>"f(x)", :H=>"α"))
 
+  while !(optimal || tired)
+    f = obj(nlp,x)
+    @info log_row(Any[k, f, α])
 
-start_time = time()
-tired = neval_obj(nlp) > max_eval || el_time > max_time
-optimal = α <= tol
-T = eltype(xk)
-status =:unknown
-@info log_header([:iter, :f, :H],[Int, T, T], hdr_override=Dict(:f=>"f(x)", :H=>"α"))
-
-while !(optimal || tired)
-	f = obj(nlp,xk)
-	@info log_row(Any[k, f, α])
-
-	for i in 1:nlp.meta.nvar
-		success = false
-		for s in [-1,1]
-			xt = copy(xk)
-			xt[i] += α * s
-			if (obj(nlp,xt) < obj(nlp,xk))
-				xk = xt
-				success = true
-				break
-			end
-		end
-		if (success == false)
-			α /= 2
-		end
-		k += 1
+    for i in 1:nlp.meta.nvar
+      success = false
+      for s in [-1,1]
+        xt = copy(x)
+        xt[i] += α * s
+        if (obj(nlp,x) < f)
+          x = xt
+          success = true
+	  break
 	end
+    end
+			
+      if (success == false)
+        α /= β
+      end
+			
+      k += 1
+    end
 		
-tired = neval_obj(nlp) > max_eval || el_time > max_time
-optimal = α <= tol
-el_time = time() - start_time
-end
+    optimal = α <= tol
+    el_time = time() - start_time
+    tired = neval_obj(nlp) > max_eval || el_time > max_time
+		
+  end
 
-if (optimal)
-	status =:first_order
-elseif (tired)
-	if (neval_obj(nlp) > max_eval && max_eval >= 0)
-		status =:max_eval
-	elseif (el_time >= max_time)
-		status =:max_time
-	end
-end
+  if optimal
+    status =:unknown
+  elseif tired
+    if (neval_obj(nlp) > max_eval && max_eval >= 0)
+      status =:max_eval
+    elseif el_time >= max_time
+      status =:max_time
+    end
+  end
 
-return GenericExecutionStats(status,nlp,solution=xk,objective=obj(nlp,xk),
-				iter = k,elapsed_time = el_time)
+  return GenericExecutionStats(status, nlp, solution=x, objective=f,
+				iter = k, elapsed_time = el_time)
 end
